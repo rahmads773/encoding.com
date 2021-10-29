93eeb4b936fbb229b3ea560cb7dc9d25138a5189
smannaba <74249644+rahmads773@users.noreply.github.com>
2021-10-29 21:04:43 +0100

    Add files via upload

---
+++ pr3.js
@@ -0,0 +1,34 @@
+let select = document.getElementById("select");
+let textarea = document.getElementById("textarea");
+let result = document.getElementById("result");
+
+textarea.addEventListener("input",(eo) => {
+    if (select.value == "decode") {
+        result.innerHTML = textarea.value;
+    } else if (select.value == "encode") {
+        result.innerHTML = btoa(textarea.value);
+    }
+    
+})
+select.addEventListener("change",(eo) => {
+    if (select.value == "decode") {
+        result.innerHTML = textarea.value;
+    } else if (select.value == "encode") {
+        result.innerHTML = btoa(textarea.value);
+    }
+    
+    
+})
+let app = document.getElementById("app");
+let cross = document.getElementById("cross");
+let btn = document.getElementById("btn");
+setTimeout(() => {
+    app.style.display ="block";
+    cross.addEventListener("click",(eo) => {
+        app.style.display ="none";
+    })
+    btn.addEventListener("click",(eo) => {
+        window.open("https://play.google.com/store/apps/details?id=com.area120.grasshopper&hl=fr&gl=US");
+    })
+
+}, 3000);
